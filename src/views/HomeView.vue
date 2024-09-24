1e85a1aae73a09b6df77be62b0cea4d71101e04e
不知火 Shiranui <supersonic@livemail.tw>
2024-09-24 22:43:12 +0800

    fix: typos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -125,7 +125,7 @@
           <div class="mt-3 sm:mt-0 sm:ml-3">
             <a href="https://web-tech-tw.github.io"
               class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-sky-700 bg-sky-100 hover:bg-sky-200 md:py-4 md:text-lg md:px-10">
-              了解更多我們社群本身
+              了解更多社群資訊
             </a>
           </div>
         </div>
